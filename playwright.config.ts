bd32d11c7cfc5421f7b46dfd32ae32f4c01508be
Marika Bertelli <marikabertelli@dxw.com>
2025-10-21 10:39:17 +0100

    config: add playwright config

---
+++ playwright.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  use: {
+    baseURL: 'http://localhost:4322',
+    trace: 'on-first-retry',
+  },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:4322',
+    reuseExistingServer: true,
+  },
+});
