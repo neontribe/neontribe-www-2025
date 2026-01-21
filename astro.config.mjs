619c30273688cfef8280769bb2a01d7631fb1cd4
Sarah Mount <sarah.mount@dxw.com>
2026-01-21 15:04:52 +0000

    Configure redirects from old site
    To preserve Neontribe SEO, this commit redirects
    URLs from the old website to their new locations.

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,15 @@ import rehypeExternalLinks from 'rehype-external-links';
 export default defineConfig({
   site: 'https://neontribe.co.uk',
   integrations: [react(), mdx()],
+  redirects: {
+    '/our-work/[...slug]': '/case-studies/[...slug]',
+    '/the-tribe/[...slug]': '/how-we-work/[...slug]',
+    '/contact-us': '/talk-to-us',
+    '/new-philanthropy-capital-case-study': '/case-studies/npc',
+    '/mind-of-my-own-case-study': '/case-studies/momo',
+    '/alexandra-rose-charity-case-study': '/case-studies/arc',
+    '/infosecurity-at-neontribe-case-study': '/case-studies/infosecurity',
+  },
   markdown: {
     remarkPlugins: [remarkGfm],
     rehypePlugins: [
